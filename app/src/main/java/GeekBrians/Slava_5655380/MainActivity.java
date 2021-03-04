ed12943f0f3afca25732ad5e671f449df2c885b5
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-03-04 20:30:26 +0300

    Решение задания 2

--- app/src/main/java/GeekBrians/Slava_5655380/MainActivity.java
+++ app/src/main/java/GeekBrians/Slava_5655380/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.task_1_activity);
+        setContentView(R.layout.task_2_activity);
     }
 }
