d2b9427ecf3dfc99a215c53e29ed313dfe86c489
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-03-14 19:03:47 +0300

    Добавление свойсва благодаря которому длинные числа на разбиваются на несколько строк

--- app/src/main/java/GeekBrians/Slava_5655380/Calculator/BinaryOperation.java
+++ app/src/main/java/GeekBrians/Slava_5655380/Calculator/BinaryOperation.java
@@ -1,5 +1,5 @@
 package GeekBrians.Slava_5655380.Calculator;
 
-public interface BinaryOperation {
+public interface  BinaryOperation {
     String binaryOperation(String leftArg, String rightArg, Operation operation);
 }
