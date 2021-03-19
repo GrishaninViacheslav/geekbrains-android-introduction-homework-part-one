4e3786dbab8585c78fab621014007319b46eea64
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-03-20 00:34:24 +0300

    Решение заданий 1 и 2

--- app/src/main/java/GeekBrians/Slava_5655380/UI/MainActivity.java
+++ app/src/main/java/GeekBrians/Slava_5655380/UI/MainActivity.java
@@ -3,7 +3,9 @@ package GeekBrians.Slava_5655380.UI;
 import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
@@ -18,6 +20,9 @@ import GeekBrians.Slava_5655380.R;
 public class MainActivity extends AppCompatActivity {
     private EditText calculatorDispaly;
     private CalculatorPresenter calculatorPresenter;
+    private static final int REQUEST_CODE_SETTING_ACTIVITY = 1;
+    private int currTheme = 0;
+    private static final String THEME_ID_BUNDLE_KEY = "THEME_ID";
 
     private void setOnClickEvent(@IdRes int id, Runnable func) {
         findViewById(id).setOnClickListener((View v) -> {
@@ -69,15 +74,66 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        findViewById(R.id.button_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent runSettings = new Intent(MainActivity.this, SettingsActivity.class);
+                Log.d("[fuck]", "R.id.button_settings currTheme: " + currTheme);
+                runSettings.putExtra(THEME_ID_BUNDLE_KEY, currTheme);
+                startActivityForResult(runSettings, REQUEST_CODE_SETTING_ACTIVITY);
+            }
+        });
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            currTheme = savedInstanceState.getInt(THEME_ID_BUNDLE_KEY);
+            Log.d("[fuck]", "currTheme: " + currTheme);
+            switch (currTheme) {
+                case 0:
+                    setTheme(R.style.Theme_Geekbrainsandroidintroductionhomework);
+                    break;
+                case 1:
+                    Log.d("[fuck]", "R.style.Theme_WhiteStyle");
+                    setTheme(R.style.Theme_WhiteStyle);
+                    break;
+
+            }
+
+        }
         setContentView(R.layout.activity_main);
         calculatorDispaly = findViewById(R.id.display);
         calculatorDispaly.setShowSoftInputOnFocus(false);
         calculatorPresenter = new CalculatorPresenter(calculatorDispaly.onCreateInputConnection(new EditorInfo()), new Calculator(), (byte) 3);
         initButtonsListeners();
     }
+<<<<<<< HEAD
 }
+=======
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != REQUEST_CODE_SETTING_ACTIVITY) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+        if (resultCode == RESULT_OK){
+            currTheme = data.getIntExtra(THEME_ID_BUNDLE_KEY, currTheme);
+            Log.d("[fuck]", "recreate currTheme: " + currTheme);
+            recreate();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle instanceState) {
+        super.onSaveInstanceState(instanceState);
+        Log.d("[fuck]", "onSaveInstanceState currTheme: " + currTheme);
+        instanceState.putInt(THEME_ID_BUNDLE_KEY, currTheme);
+
+    }
+}
+>>>>>>> d7ab3d9 (Решение заданий 1 и 2)
